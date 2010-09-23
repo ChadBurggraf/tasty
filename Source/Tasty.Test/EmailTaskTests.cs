afa1facb451de2c4eea9feaa15027842227d90a7
Chad Burggraf <chad.burggraf@gmail.com>
2010-09-22 23:05:43 -0700

    Created test for the email MSBuild task.

---
+++ Source/Tasty.Test/EmailTaskTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.BuildEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasty.Build;
+
+namespace Tasty.Test
+{
+    [TestClass]
+    public class EmailTaskTests
+    {
+        [TestMethod]
+        public void EmailTask_MsBuild()
+        {
+            const string ProjectFile = "Email.proj";
+
+            if (File.Exists(ProjectFile))
+            {
+                Assert.IsTrue(Engine.GlobalEngine.BuildProjectFile(ProjectFile));
+            }
+        }
+    }
+}
